49ca9c546475a8790e05b38604c35202baee3072
ryan <rhynogolfer42@gmail.com>
2024-03-04 18:18:03 -0800

    Adds myfile.md

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+# Full CRUD for Contact in models.py
+
+from flask import request, jsonify
+from config import app,db
+from models import Contact
+
+
+@app.route("/contacts", methods=["GET"])
+def get_contact():
+    contacts = Contact.query.all()
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts})
+
+@app.routes("/create_contact", methods=["POST"])
+def create_contact():
+
+
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
+
+    app.run(debug=True)
