47959ca6d78772c32a74e51c813b0f3d0a685a41
ryan <rhynogolfer42@gmail.com>
2024-03-04 19:34:11 -0800

    Adds myfile.md

---
+++ frontend/src/ContactForm.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react"
+
+const ContactForm = ({ }) => {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+
+    const onSubmit = () => {
+        e.preventDefault()
+
+        const data = {
+            firstName,
+            lastName,
+            email
+        }
+        const url = "https://127.0.0.1:5000/cdreate_contact"
+        const options = {
+            method: "Post",
+            headers: {
+                "content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        const response = await fetch(url, options)
+        if (response.status !== 201 && response.status !== 200) {
+            const message = await response.json()
+            alert(message.message)
+        }
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label htmlFor="firstName">First Name:</label>
+                <input
+                    type="text"
+                    id="firstName"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="lastName">Last Name:</label>
+                <input
+                    type="text"
+                    id="lastName"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <button type="submit">Create Contact</button>
+        </form>
+    );
+};
+
+export default ContactForm;
